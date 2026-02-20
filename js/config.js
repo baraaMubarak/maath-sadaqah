a1813fdbe604505abb9fd145fcb2d9f1ea01d952
Baraa M Mubarak <88498624+baraaMubarak@users.noreply.github.com>
2026-02-21 01:05:29 +0200

    Add Firebase configuration and prayer arrays

---
+++ js/config.js
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC_HXOPcbL80yEbWTx8KFJ5DS2lun5doJY",
+    authDomain: "maath-sadaqah.firebaseapp.com",
+    projectId: "maath-sadaqah",
+    storageBucket: "maath-sadaqah.firebasestorage.app",
+    messagingSenderId: "563608713392",
+    appId: "1:563608713392:web:2f0bd4f0fbdd8790f0e375",
+    measurementId: "G-5PN53EB571",
+    databaseURL: "https://maath-sadaqah-default-rtdb.europe-west1.firebasedatabase.app"
+};
+
+export const adhkar = ['سبحان الله', 'الحمد لله', 'الله أكبر', 'لا إله إلا الله', 'أستغفر الله'];
+
+export const duas = [
+    'اللهم اغفر لمعاذ وارحمه وعافه واعف عنه',
+    'اللهم اجعل القرآن ربيع قلبه ونور صدره',
+    'اللهم أدخله الجنة بغير حساب ولا سابقة عذاب',
+    'اللهم اجمعنا به في الفردوس الأعلى',
+    'اللهم أنس وحشته في القبر ونور له مضجعه'
+];
