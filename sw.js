91c07954ea0436ae84a79799d40e3ecba19df297
Baraa M Mubarak <88498624+baraaMubarak@users.noreply.github.com>
2026-02-26 00:35:33 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'maath-v1';
+const STATIC_ASSETS = [
+    '/maath-sadaqah/',
+    '/maath-sadaqah/index.html',
+    '/maath-sadaqah/css/style.css',
+    '/maath-sadaqah/js/main.js',
+    '/maath-sadaqah/js/config.js',
+    '/maath-sadaqah/js/db.js',
+    '/maath-sadaqah/js/tasbih.js',
+    '/maath-sadaqah/js/ui.js',
+    'https://fonts.googleapis.com/css2?family=Cairo:wght@300;400;500;600&family=El+Messiri:wght@400;500;600&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+// تثبيت الـ Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(STATIC_ASSETS);
+        })
+    );
+    self.skipWaiting();
+});
+
+// تفعيل الـ Service Worker
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+// جلب البيانات
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
